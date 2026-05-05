8b1a3a8b94af88c3f8965e79f90035f02f458191
Mazen <Mazenatschool@gmail.com>
2026-05-05 02:57:25 -0400

    Update petRoutes.js
    Fix route paths

--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -4,9 +4,10 @@ import requireAuth from '../middleware/requireAuth.js';
 import isAdmin from '../middleware/requireAuth.js'
 const router = Router();
 
-router.get('/', petController.getAllPets)
-router.post('/pet', requireAuth, isAdmin, petController.addPet)
-router.put('/pet/:id', requireAuth, isAdmin, petController.updatePet)
-router.delete('/pet/:id', requireAuth, isAdmin, petController.deletePet)
+// RESTful pet routes
+router.get('/', petController.getAllPets) 
+router.post('/', requireAuth, isAdmin, petController.addPet)
+router.put('/:id', requireAuth, isAdmin, petController.updatePet)
+router.delete('/:id', requireAuth, isAdmin, petController.deletePet)
 
 export default router
