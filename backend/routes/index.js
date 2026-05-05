1b37d9ca355dd74d9b94d8069be3af156b854677
Mazen <Mazenatschool@gmail.com>
2026-05-05 07:09:19 +0000

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes.js';
-import profileInfoRoutes from './profileRoutes.js';
+import profileRoutes from './profileRoutes.js';
 import cartRoutes from './cartRoute.js';
 import orderRoutes from './orderRoute.js';
 import petsRoutes from './petRoutes.js';
@@ -11,7 +11,7 @@ const router = Router();
 
 router.use('/auth',        authRoutes);
 router.use('/pets',        petsRoutes);
-router.use('/profile',     requireAuth, profileInfoRoutes);
+router.use('/profile',     requireAuth, profileRoutes); // really should be /User
 router.use('/cart',        cartRoutes);
 router.use('/order',       orderRoutes);
 router.use('/orderRecord', orderRecordRoutes);
